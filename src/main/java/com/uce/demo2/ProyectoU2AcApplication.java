6085accc5e394e416038ddc2b0befaedef575b27
AlexCondor <alexguerron@gmail.com>
2022-06-21 19:52:55 -0500

    talller13

---
+++ src/main/java/com/uce/demo2/ProyectoU2AcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2AcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2AcApplication.class, args);
+	}
+
+}
